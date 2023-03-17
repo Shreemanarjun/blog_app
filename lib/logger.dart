bdca2a25f73b594185ab2ce9eac3efaccb74dfe9
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-03-17 20:03:08 +0530

    🐛 fix logger comaptibility issue in web

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:platform_info/platform_info.dart';
 import 'package:talker/talker.dart';
 
 final talker = Talker(
@@ -8,7 +7,7 @@ final talker = Talker(
     output: debugPrint, // add this to avoid this issue.
     settings: TalkerLoggerSettings(
       // disable colors in iOS due to https://github.com/flutter/flutter/issues/20663
-      enableColors: !Platform.isIOS,
+      enableColors: !Platform.I.isIOS,
     ),
   ),
 );
