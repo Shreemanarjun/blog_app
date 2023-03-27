8006a7cd0e18b63be49f2399fdcf6e0f0ad07a7a
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-03-27 22:36:49 +0530

    🚨 add riverpod lint

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:blog_app/l10n/l10n.dart';
+import 'package:blog_app/logger.dart';
 import 'package:blog_app/router/router_pod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,12 +11,22 @@ class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final approuter = ref.watch(autoRouterPod);
+    ref.listen(isGuardCheckingPod, (previous, next) {
+      talker.debug('guard checking $next');
+    });
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
           accentColor: const Color(0xFF13B9FF),
+        ),
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            // replace default CupertinoPageTransitionsBuilder with this
+            TargetPlatform.iOS: NoShadowCupertinoPageTransitionsBuilder(),
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          },
         ),
       ),
       routerConfig: approuter.config(),
