1c17e79dccba1d70973b51f377bffbfbec58fb77
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-05-11 03:29:29 +0530

    👷 fix ios build

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -3,7 +3,7 @@
 export "FLUTTER_ROOT=/Users/shreemanarjunsahu/fvm/versions/stable"
 export "FLUTTER_APPLICATION_PATH=/Users/shreemanarjunsahu/Documents/blog_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/shreemanarjunsahu/Documents/blog_app/lib/main_development.dart"
+export "FLUTTER_TARGET=/Users/shreemanarjunsahu/Documents/blog_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
